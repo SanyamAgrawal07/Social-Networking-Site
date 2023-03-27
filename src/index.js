e0bfb3090074e4b59e8faebf460f822c5e5c1593
Sanyam07 <agrawalsanyam07@gmail.com>
2023-03-27 11:26:05 +0530

    npm libraries installed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const socketio = require('socket.io')
 const http = require('http')
 const path = require('path')
@@ -6,6 +7,35 @@ require('dotenv').config()
 const { generateMessage,generateLocationMessage } = require('./utils/messages')
 const { addUser,removeUser,getUser,getUsersInRoom } = require('./utils/users')
 // const url = require('url')
+// const { Neo4jGraphQL } = require("@neo4j/graphql");
+// const neo4j = require("neo4j-driver");
+
+// const typeDefs = gql`
+//     type Movie {
+//         title: String
+//         actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
+//     }
+
+//     type Actor {
+//         name: String
+//         movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+//     }
+// `;
+
+// const driver = neo4j.driver(
+//     process.env.NEO4J_URI,
+//     neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
+// );
+
+// const neoSchema = new Neo4jGraphQL({ typeDefs, driver })
+
+// const graphqlServer = new ApolloServer({
+//     schema: neoSchema.schema
+// }) 
+
+// graphqlServer.listen(({url})=>{
+//     console.log(`GraphQl server ready at ${url}`)
+// })
 
 const app = express()
 const authRouter = require('./routes/auth.routes.js')
